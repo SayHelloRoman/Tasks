410de237ed7b604386790bca3bb2ce3383301efc
Roman <68429513+SayHelloRoman@users.noreply.github.com>
2021-02-28 16:50:34 +0200

    Update main.cpp

--- task_three/main.cpp
+++ task_three/main.cpp
@@ -5,24 +5,24 @@
 using namespace std;
 
 int factorial(int number){
-	int f = 1;
+    int f = 1;
 
-	for(int i = 1;i <= number;i++){
-		f = f * i;
-	}
-	return f;
-	
+    for(int i = 1;i <= number;i++){
+        f = f * i;
+    }
+    return f;
+    
 }
 
 int main()
 {
-	char str[1000];
-	ofstream fout("output.txt", ios_base::out | ios_base::trunc);
-	ifstream fin("input.txt");
+    char str[1000];
+    ofstream fout("output.txt", ios_base::out | ios_base::trunc);
+    ifstream fin("input.txt");
 
-	fin >> str;
-	fout << factorial(atoi(str));
+    fin >> str;
+    fout << factorial(atoi(str));
 
-	fin.close();
-	fout.close();
+    fin.close();
+    fout.close();
 }
