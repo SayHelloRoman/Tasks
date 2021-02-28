55201183e3a777d72523b42e9bcad8c38756e61e
Roman <68429513+SayHelloRoman@users.noreply.github.com>
2021-02-28 22:16:47 +0200

    Add files via upload

---
+++ task_four/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int proizvedenie(int n){
+    string str = to_string(n);
+    int result = 1;
+    for(int i = 0; i < str.length(); i++){
+        result = (str[i] - '0') * result;
+    }
+    return result;
+}
+int main(){
+    char str[10];
+    int write = -1;
+    ofstream fout("output.txt", ios_base::out | ios_base::trunc);
+    ifstream fin("input.txt");
+    fin >> str;
+    int number = atoi(str);
+
+    for(int i = 1; i <= 100; i++){
+        int res = proizvedenie(i);
+        if(res == number){
+            write = i;
+            break;
+            }
+        }
+
+    fout << write;
+    fin.close();
+    fout.close();
+}
